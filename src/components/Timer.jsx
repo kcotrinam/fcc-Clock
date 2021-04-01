144af00993264da1a7b13ae6f88e50c43f5240b8
Kevin Cotrina <kcotrinma92@gmail.com>
2021-04-01 00:26:06 -0500

    Add link between session timer and timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react';
+
 const Timer = ({ currentTime }) => {
-	return <h1>{currentTime}</h1>;
+	const [timer, setTimer] = useState({
+		minutes: '0',
+		seconds: '00',
+	});
+
+	useEffect(() => {
+		const min = currentTime % 60;
+		const seconds = '00';
+
+		setTimer({
+			minutes: `${min}`,
+			seconds: `${seconds}`,
+		});
+	}, [currentTime]);
+
+	return (
+		<div className='timer'>
+			<h2>
+				{timer.minutes} : {timer.seconds}
+			</h2>
+		</div>
+	);
 };
 
 export default Timer;
